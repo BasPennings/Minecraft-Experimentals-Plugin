afe7473712c2dd49978bcc07084aa45d52e38a54
BasPennings <152502925+BasPennings@users.noreply.github.com>
2024-03-30 12:55:37 +0100

    Added generated plugin files

---
+++ src/main/java/bazzeman/expirimentals/Expirimentals.java
@@ -0,0 +1,17 @@
+package bazzeman.expirimentals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Expirimentals extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
